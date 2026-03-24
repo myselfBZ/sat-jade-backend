bed7daa24cb8da901e52499d6c7afabbe154f9f7
Bobirmirzo <boburforfun@gmail.com>
2026-03-24 17:14:57 +0500

    refactor(tests): gemini api key now fetched from env var

--- internal/llm/gemini_test.go
+++ internal/llm/gemini_test.go
@@ -1,11 +1,22 @@
 package llm
 
 import (
-	"log"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 func Test_Prompting(t *testing.T) {
+	if err := godotenv.Load(".env"); err != nil {
+		t.Fatal(".env file not found")
+	}
+
+	GEMINI_API_KEY := os.Getenv("GEMINI_API_KEY")
+	if GEMINI_API_KEY == "" {
+		t.Fatal("GEMINI_API_KEY env var not found")
+	}
+
 	stats := PracticeOverviewParams{
 		CorrectAnswers: 97,
 		Mistakes: MistakeCountByDomain{
@@ -14,13 +25,11 @@ func Test_Prompting(t *testing.T) {
 	}
 
 	gemini := &GeminiLLM{
-		apikey: "AIzaSyDsOClmYzPr5ydj_LpKg7SNvkTzNoJqRIY",
+		apikey: GEMINI_API_KEY,
 	}
 
-	overview, err := gemini.GeneratePracticeOverview(&stats)
+	_, err := gemini.GeneratePracticeOverview(&stats)
 	if err != nil {
 		t.Fatalf("error isn't nil: %v", err)
 	}
-	log.Println(overview.Overview)
-	log.Println(overview.Motivation)
 }
